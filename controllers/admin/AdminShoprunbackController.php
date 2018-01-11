fe7df8435b7913c18a23c639d1d568201c275f15
Paul Girardin <paul.girardin@eemi.com>
2018-01-11 14:55:22 +0100

    Refactor AdminController initcontent

--- controllers/admin/AdminShoprunbackController.php
+++ controllers/admin/AdminShoprunbackController.php
@@ -11,6 +11,7 @@ class AdminShoprunbackController extends ModuleAdminController
         $this->addCSS(_PS_MODULE_DIR_ . $this->module->name . '/views/css/srbGlobal.css');
         $this->addCSS(_PS_MODULE_DIR_ . $this->module->name . '/views/css/admin/header.css');
         $this->addCSS(_PS_MODULE_DIR_ . $this->module->name . '/views/css/admin/override.css');
+        $this->addCSS('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
         $this->actionResult = false;
         $this->tabUrl = Context::getContext()->link->getAdminLink('AdminShoprunback');
 
@@ -56,20 +57,10 @@ class AdminShoprunbackController extends ModuleAdminController
         $link = new Link();
         parent::initContent();
 
-        $this->context->smarty->assign('token', Configuration::get('srbtoken'));
-        $this->context->smarty->assign('shoprunbackURL', $this->module->url);
-        $this->context->smarty->assign('shoprunbackURLProd', $this->module->urlProd);
-        $this->context->smarty->assign('shoprunbackAPIURL', Synchronizer::SRB_API_URL);
-        $this->context->smarty->assign('srbManager', $this->tabUrl);
-        $this->context->smarty->assign('asyncCall', $this->tabUrl . '&action=asyncCall');
-        $this->context->smarty->assign('link', $link);
-
-        $conditionsToSend = '';
         $pages = 1;
         $itemType = (isset($_GET['itemType'])) ? $_GET['itemType'] : 'returns';
         $items = [];
         $template = 'srbManager';
-        $externalLink = $this->module->url;
         $message = '';
         $this->context->smarty->assign('itemType', $itemType);
 
@@ -91,68 +82,81 @@ class AdminShoprunbackController extends ModuleAdminController
 
             $this->addCSS(_PS_MODULE_DIR_ . $this->module->name . '/views/css/admin/config.css');
         } else {
-            switch ($itemType) {
-                case 'returns':
-                    if (Tools::getValue('orderId') !== false) {
-                        $items = SRBShipback::getLikeOrderIdByCreateDate(Tools::getValue('orderId'));
-                    } elseif (Tools::getValue('customer') !== false) {
-                        $items = SRBShipback::getLikeCustomerByCreateDate(Tools::getValue('customer'));
-                    } else {
-                        $items = SRBShipback::getAllByCreateDate();
-                    }
-
-                    $externalLink .= '/shipbacks/';
-
-                    $actionUrl = Context::getContext()->link->getAdminLink('AdminShoprunback') . '&itemType=returns';
-                    $this->context->smarty->assign('actionUrl', $actionUrl);
-                    $this->context->smarty->assign('searchOrderId', Tools::getValue('orderId'));
-                    $this->context->smarty->assign('searchCustomer', Tools::getValue('customer'));
-                    break;
-                case 'products':
-                    $items = SRBProduct::getAllWithMapping();
-                    $externalLink .= '/products/';
-                    break;
-                case 'orders':
-                    $items = SRBOrder::getAllWithMapping();
-                    $externalLink .= '/orders/';
-                    break;
-                case 'brands':
-                    $items = SRBBrand::getAllWithMapping();
-                    $externalLink .= '/brands/';
-                    break;
-            }
-
-            $itemsByPage = 20;
-            $currentPage = (isset($_GET['currentPage'])) ? $_GET['currentPage'] : 1;
-
-            $countItems = count($items);
-            $pages = ceil($countItems / $itemsByPage);
-            $currentPage = ($currentPage <= $pages) ? $currentPage : 1;
-
-            $itemMin = ($currentPage - 1) * $itemsByPage;
-            $itemMax = $currentPage * $itemsByPage;
-            $itemsToShow = [];
-            for ($itemMin; $itemMin < $itemMax; $itemMin++) {
-                if (isset($items[$itemMin])) {
-                    $itemsToShow[] = $items[$itemMin];
-                }
-            }
-
-            $this->context->smarty->assign('currentPage', $currentPage);
-            $this->context->smarty->assign('pages', $pages);
-            $this->context->smarty->assign('items', $itemsToShow);
-            $this->context->smarty->assign('conditionsToSend', $conditionsToSend);
-            $this->context->smarty->assign('externalLink', $externalLink);
-            $this->addJs(_PS_MODULE_DIR_ . $this->module->name . '/views/js/admin/srbManager.js');
-            $this->addCSS(_PS_MODULE_DIR_ . $this->module->name . '/views/css/admin/srbManager.css');
+            $this->getItems($itemType);
         }
 
-        $this->addCSS(_PS_MODULE_DIR_ . $this->module->name . '/views/css/admin/override.css');
-        $this->addCSS('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-
+        $this->context->smarty->assign('token', Configuration::get('srbtoken'));
+        $this->context->smarty->assign('shoprunbackURL', $this->module->url);
+        $this->context->smarty->assign('shoprunbackURLProd', $this->module->urlProd);
+        $this->context->smarty->assign('shoprunbackAPIURL', Synchronizer::SRB_API_URL);
+        $this->context->smarty->assign('srbManager', $this->tabUrl);
+        $this->context->smarty->assign('asyncCall', $this->tabUrl . '&action=asyncCall');
+        $this->context->smarty->assign('link', $link);
         $this->context->smarty->assign('template', $template);
         $this->context->smarty->assign('message', $message);
         $this->setTemplate('../../../../modules/' . $this->module->name . '/views/templates/admin/layout.tpl');
+    }
+
+    private function getItems ($itemType = 'returns') {
+        $externalLink = $this->module->url;
+
+        switch ($itemType) {
+            case 'returns':
+                if (Tools::getValue('orderId') !== false) {
+                    $items = SRBShipback::getLikeOrderIdByCreateDate(Tools::getValue('orderId'));
+                } elseif (Tools::getValue('customer') !== false) {
+                    $items = SRBShipback::getLikeCustomerByCreateDate(Tools::getValue('customer'));
+                } else {
+                    $items = SRBShipback::getAllByCreateDate();
+                }
+
+                $externalLink .= '/shipbacks/';
+
+                $actionUrl = Context::getContext()->link->getAdminLink('AdminShoprunback') . '&itemType=returns';
+                $this->context->smarty->assign('actionUrl', $actionUrl);
+                $this->context->smarty->assign('searchOrderId', Tools::getValue('orderId'));
+                $this->context->smarty->assign('searchCustomer', Tools::getValue('customer'));
+                break;
+            case 'products':
+                $items = SRBProduct::getAllWithMapping();
+                $externalLink .= '/products/';
+                break;
+            case 'orders':
+                $items = SRBOrder::getAllWithMapping();
+                $externalLink .= '/orders/';
+                break;
+            case 'brands':
+                $items = SRBBrand::getAllWithMapping();
+                $externalLink .= '/brands/';
+                break;
+        }
+
+        $this->getPagination($items);
+
+        $this->context->smarty->assign('externalLink', $externalLink);
+        $this->addJs(_PS_MODULE_DIR_ . $this->module->name . '/views/js/admin/srbManager.js');
+        $this->addCSS(_PS_MODULE_DIR_ . $this->module->name . '/views/css/admin/srbManager.css');
+    }
+
+    private function getPagination ($items = []) {
+        $countItems = count($items);
+        $itemsByPage = 10;
+        $pages = ceil($countItems / $itemsByPage);
+        $currentPage = (isset($_GET['currentPage'])) ? $_GET['currentPage'] : 1;
+        $currentPage = ($currentPage <= $pages) ? $currentPage : 1;
+
+        $itemMin = ($currentPage - 1) * $itemsByPage;
+        $itemMax = $currentPage * $itemsByPage;
+        $itemsToShow = [];
+        for ($itemMin; $itemMin < $itemMax; $itemMin++) {
+            if (isset($items[$itemMin])) {
+                $itemsToShow[] = $items[$itemMin];
+            }
+        }
+
+        $this->context->smarty->assign('pages', $pages);
+        $this->context->smarty->assign('currentPage', $currentPage);
+        $this->context->smarty->assign('items', $itemsToShow);
     }
 
     private function getConfigFormValues () {
