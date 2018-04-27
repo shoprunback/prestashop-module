e2216a8b19b96d05b438290f8f81f90b99636b2d
Leo Martin <leo@shoprunback.com>
2018-04-27 16:22:31 +0200

    Refacto js

--- views/js/admin/tab-1.7.js
+++ views/js/admin/tab-1.7.js
@@ -1,15 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   var menu = document.getElementsByClassName('menu')[0];
-  var menuModule = document.getElementsByClassName('main-menu')[0];
-  var url = 'controller=AdminShoprunback';
   if (!menu) {
-    menu = menuModule;
+    menu = document.getElementsByClassName('main-menu')[0];
   }
   for (var i = 0; i < menu.childNodes.length; i++) {
     var node = menu.childNodes[i];
     for (var j = 0; j < node.childNodes.length; j++) {
       var a = node.childNodes[j];
-      if (a.href && a.href.indexOf(url) != -1) {
+      if (a.href && a.href.indexOf('controller=AdminShoprunback') != -1) {
         a.childNodes[1].innerHTML = 'sync';
         return;
       }
