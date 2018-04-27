bfdb2a9247c75d459ad240a9d057e608200c0f54
Leo Martin <leo@shoprunback.com>
2018-04-27 16:18:33 +0200

    icon working on 1.7

--- views/js/admin/tab-1.7.js
+++ views/js/admin/tab-1.7.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  document.getElementById('subtab-AdminShoprunback').childNodes[1].childNodes[1].innerHTML = 'sync';
+  var menu = document.getElementsByClassName('menu')[0];
+  var menuModule = document.getElementsByClassName('main-menu')[0];
+  var url = 'controller=AdminShoprunback';
+  if (!menu) {
+    menu = menuModule;
+  }
+  for (var i = 0; i < menu.childNodes.length; i++) {
+    var node = menu.childNodes[i];
+    for (var j = 0; j < node.childNodes.length; j++) {
+      var a = node.childNodes[j];
+      if (a.href && a.href.indexOf(url) != -1) {
+        a.childNodes[1].innerHTML = 'sync';
+        return;
+      }
+    }
+  }
 });
