eb6750365b00351d4ffcdb5943a37e80ff4273a0
PaulGirardin <paul@shoprunback.com>
2018-05-24 17:21:30 +0200

    Bump version

--- shoprunback.php
+++ shoprunback.php
@@ -54,7 +54,7 @@ class ShopRunBack extends Module
         // Mandatory parameters
         $this->name = 'shoprunback';
         $this->author = 'ShopRunBack';
-        $this->version = '1.0.10';
+        $this->version = '1.0.8';
         $this->ps_versions_compliancy = array('min' => '1.6.0.9');
         $this->tab = 'administration';
         $this->tabs = [
